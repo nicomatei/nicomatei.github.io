9df194bda2ecb79adcfaa89e8486ec762ff218cd
Nicoleta Matei <mateinikoleta10@gmail.com>
2022-07-12 10:35:55 +0300

    add 2 more pages

--- index.js
+++ index.js
@@ -12,9 +12,40 @@ var age = 12;
 // console.info("My name is " + myName + ", I'm " + age + " years old.");
 
 var jobTitle = "<span>Student @</span> FastTrackIT";
+
 var mottoElement = document.getElementById("motto");
 
 console.info(mottoElement);
 console.warn(mottoElement.innerHTML);
 
 mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
+
+function showHomePage() {
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "block";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+}
+
+function showProjectsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+
+function showLanguagesPage() {
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+
+showHomePage();
